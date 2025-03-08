18561dea23c2ec96a1313822a5763f2de6c8f934
Ferchus <ferchusgames@protonmail.com>
2025-03-08 11:56:04 -0600

    Bindings to the AnimationController using UnityEvents

---
+++ Assets/Scripts/Utilities/Bindings/IntBinding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Utilities.Bindings
+{
+    [Serializable]
+    public class IntBinding : ValueBinding<int>
+    {
+        public override int Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                if (_animator && !string.IsNullOrEmpty(_parameter))
+                {
+                    _animator.SetInteger(_parameter, _value);
+                }
+            }
+        }
+    }
+}
