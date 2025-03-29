299339b520745a9f43e5e02cc00626eb3c59dc54
Ferchus <ferchusgames@protonmail.com>
2025-03-29 12:00:07 -0600

    Fix http bad request

--- Assets/Scripts/GameApp.cs
+++ Assets/Scripts/GameApp.cs
@@ -5,16 +5,44 @@ using Character.Views;
 using Cysharp.Threading.Tasks;
 using DefaultNamespace;
 using Utilities.AddressableLoader;
+using Utilities.DataAPI;
+
+struct CharacterDataByStyleName
+{
+    public string styleName;
+}
 
 public class GameApp : IGameApp
 {
     private CancellationTokenSource _gameTokenSource = new();
-
+    
     public async UniTaskVoid StartApp()
     {
-        var characterView = 
-            await AddressableLoader.InstantiateAsync<ICharacterView>("basePlayer");
-        ICharacterData characterData = new CharacterDataDummy();
+        var characterView = await AddressableLoader.InstantiateAsync<ICharacterView>("basePlayer");
+
+        var characterDataQueryByStyleName = @"query Query($styleName: String!) {
+          CharacterDataByStyleName(styleName: $styleName) {
+            jumpForce
+            moveSpeed
+            styleName
+          }
+        }";
+        
+        var characterDataByStyleNameVariables = new CharacterDataByStyleName()
+        {
+            styleName = "basePlayer"
+        };
+
+        var fullQueryCharacterData = new GraphQlQuery()
+        {
+            query = characterDataQueryByStyleName,
+            variables = characterDataByStyleNameVariables
+        };
+        
+        ICharacterData characterData = await GraphqlUtils.GetModel<CharacterData>(
+            fullQueryCharacterData, 
+            "Query", 
+            _gameTokenSource.Token);
         
         ICharacterBaseController characterBaseController =
             new CharacterBaseController(characterView, characterData, _gameTokenSource.Token);
